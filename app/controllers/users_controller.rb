97f528bdd49c55a9d6d8fd0e3ff347cac83159c9
github username <github email address>
2020-04-10 23:46:24 -0700

    create show page and changed stylings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,31 @@
 class UsersController < ApplicationController
-    # before_action :verified_user, only: [:new, :create]
-    
+
+    def index
+        @users = User.all
+    end
+
     def new
         @user = User.new
     end
 
     def create 
-
+        if (@user = User.create(user_params))
+            session[:user_id] = @user.id 
+            # binding.pry
+            redirect_to user_path(@user)
+        else
+            render 'new'
+        end
     end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        # binding.pry
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:id, :full_name, :username, :password, :phone_number, :email, :number_of_guests, :budget, :event_type, )
+    end 
 end
